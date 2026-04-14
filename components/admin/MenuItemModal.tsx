35c504b1703dbaa5f59ac46dcf7d34d7e09d656b
okonomiyakib <raifu.20120405@gmail.com>
2026-04-14 17:04:25 +0900

    Cloudinary設定値をフォールバックとして直接記載

--- components/admin/MenuItemModal.tsx
+++ components/admin/MenuItemModal.tsx
@@ -53,12 +53,12 @@ export function MenuItemModal({
     const toastId = toast.loading("Cloudinaryにアップロード中...");
 
     try {
-      const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
-      const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
+      const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME ?? "dadqxvtpy";
+      const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET ?? "ml_default";
 
       const formData = new FormData();
       formData.append("file", file);
-      formData.append("upload_preset", uploadPreset!);
+      formData.append("upload_preset", uploadPreset);
 
       const res = await fetch(
         `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
